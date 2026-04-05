6bf06f323ea8f99c0b0bfc4fafd3f091e3f155e2
madinabolat <madinabolat01@gmail.com>
2026-04-05 18:19:20 -0400

    add routes to app.jsx, set up home and register pages

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { useNavigate } from 'react-router-dom'
+
+function Home () {
+    const navigate = useNavigate()
+    return (
+            <>
+                <section>
+                    <div>
+                        <h1>Welcome to Learner</h1>
+                    </div>
+                    <button onClick={() => navigate("/register")}>
+                        Register
+                    </button>
+                    <button>
+                        Sign in
+                    </button>
+                </section>
+            </>
+    )
+}
+
+export default Home
