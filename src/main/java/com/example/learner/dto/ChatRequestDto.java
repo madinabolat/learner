a6096138520b62e840ad1d229331a03d5691f69e
madinabolat <madinabolat01@gmail.com>
2026-05-26 21:28:48 -0400

    Week 6 part 1: add api_key to application.properties, add Chat DTO and Chat Service, Chat Controller

---
+++ src/main/java/com/example/learner/dto/ChatRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.learner.dto;
+
+public class ChatRequestDto {
+    private String request;
+
+    public String getRequest() {
+        return request;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+}
