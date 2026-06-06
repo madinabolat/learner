6070e96b8d2bdb5e0b7e587f8a98c0c5a6d68f57
madinabolat <madinabolat01@gmail.com>
2026-06-05 23:32:50 -0400

    Update JwtUtil.java

--- src/main/java/com/example/learner/util/JwtUtil.java
+++ src/main/java/com/example/learner/util/JwtUtil.java
@@ -5,17 +5,25 @@ import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 @Component
 public class JwtUtil {
     @Value("${SECRET_KEY}")
     private String secret;
 
     String generateToken(String username){
+        Date now = new Date();
+        //generate key: 
+//        Key key = secret.getBytes();
         return Jwts.builder()
-                .addClaims()
-                .setPayload()
-                .setIssuedAt()
-                .setExpiration();
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(Date.from(now.toInstant().plus(1, ChronoUnit.HOURS)))
+//                .signWith(secret)
+                .compact();
     }
 
     boolean validateToken(String token){
